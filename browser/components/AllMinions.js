2023b20f220e4910c64b9eb25b48a225a7bf23bf
Dan <120590237+ender1311@users.noreply.github.com>
2023-03-29 13:15:52 -0400

    update

--- browser/components/AllMinions.js
+++ browser/components/AllMinions.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
 import { deleteMinionThunk } from '../store/minions'; 
+import Minion from './Minion';
 
 const AllMinions = props => {
   const minions = props.minions.map(minion => {
@@ -11,7 +12,7 @@ const AllMinions = props => {
       <div className="minion-grid" key={minion.id}>
         <Link to={`/minions/${minion.id}`} >
           <img className="button minion-thumbnail" src={`public/img/${minion.id}.png`} alt="minion" />
-          <p>{name}</p>
+          <p style={{textAlign:'left'}}>{name}</p>
           <p>ID #{minion.id}</p>
         </Link>
         <img onClick={() => props.deleteMinion(minion.id)}  className="button x-button" src="public/img/x_button.svg" alt="" />
@@ -27,7 +28,8 @@ const AllMinions = props => {
           { minions }
           <div id="add-minion-button-grid" className="minion-grid">
             <Link to="/minions/new">
-              <img id="add-minion-button" className="button" src="public/img/add_button.svg" alt="" />
+              <button className="add_minion_button" style = {{ borderRadius: '50px', backgroundColor: '#ff4f58', color:'white' , cursor: 'pointer', fontSize:'24px'}} >Add Minion</button>
+              {/* <img id="add-minion-button" className="button" src="public/img/add_button.svg" alt="" /> */}
             </Link>
           </div>
         </div>
